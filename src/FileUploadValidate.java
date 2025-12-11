17f9becf3e80379691bec6709eb55bf764a2e3d2
Milinda Dissanayake <150835052+MilindaDissanayake@users.noreply.github.com>
2025-12-11 13:57:59 +0530

    implement CSV file upload function..

--- src/FileUploadValidate.java
+++ src/FileUploadValidate.java
@@ -1,3 +1,27 @@
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class FileUploadValidate {
-//    Write functions realted to csv file browse and validate
+    private File selectedCsvFile;
+
+    public boolean fileUpload(Stage primaryStage, Label errorMsg){
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+        File file = fc.showOpenDialog(primaryStage);
+
+        if (file != null) {
+            selectedCsvFile = file;
+            System.out.println("Selected CSV: " + file.getAbsolutePath());
+            errorMsg.setVisible(false);
+            return true;
+        }
+        return false;
+    }
+
 }
